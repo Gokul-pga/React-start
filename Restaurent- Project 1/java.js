e5ec17922f2445ed2212f3d41c597373d8194657
Gokul-pga <118168471+Gokul-pga@users.noreply.github.com>
2023-04-06 19:35:50 +0530

    Add files via upload

---
+++ Restaurent- Project 1/java.js
@@ -0,0 +1,54 @@
+let myBtn = document.getElementById("myBtn");
+
+// alert("hello")
+
+// function myFunction() {
+//     document.getElementById("top-move").innerHTML="hello";
+// }
+
+
+/*---  button fot scroll to top of the website  ---- */
+
+function myFunctionTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+// let hour = new Date().getHours;
+
+// if (hour >= 0 && hour <= 12){
+//     alert("Hey, Gud Morning")
+// }
+// else if (hour >= 13 && hour <= 18){
+//     alert("Hey, Gud Afternoon")
+// }
+// else{
+//     alert("Hey, Gud Evening")
+// }
+
+const car = ["Benz", "Audi", "BMW"];
+car.push("volkswgon", "Skoda");
+// console.log(car.sort());
+// console.log(car,car.shift());
+console.log(car.unshift("lambogni"));
+console.log(car);
+
+// const vehicle = new Object()
+
+// vehicle.name = "BMW";
+// vehicle.model="5Series";
+// vehicle.millage= "15km\lr"
+
+// console.log(vehicle);
+
+// function vehicles(brand, sedan, topspeed){
+// this.brand=brand;
+// this.sedan=sedan;
+// this.topspeed=topspeed;
+// }
+
+// const dream = new vehicles("benz", "c-class", "250+")
+// console.log(dream);
+
+
